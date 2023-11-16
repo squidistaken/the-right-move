de42f2873f44e5e9bfe1ff95cba31e7ab2bdc46c
Marcus Persson <marcusper2003@gmail.com>
2023-11-16 23:08:38 +0100

    add: proper super jump and dash

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,24 +1,26 @@
 extends CharacterBody2D
 
 const speed = 400
-const dash_multiplier = 5
-@export var jump_velocity = -2000
-@export var acc = 50
-@export var fric = 70
-@export var grav = 120
+@export var dash_multiplier = 2000
+const jump_velocity = -2000
+const acc = 50
+const fric = 70
+const grav = 120
 const cooldown = 0.5
 
 var has_jumped = false
 var has_dashed = false
-var has_super_jumped = false
+var can_super_jump = false
 
 func _physics_process(_delta):
 	var input_dir: Vector2 = input()
 	
 	if input_dir != Vector2.ZERO:
 		velocity = velocity.move_toward(speed * input_dir, acc)
+		if Input.is_action_pressed("super_jump"):
+			velocity.x /= 2
 		if Input.is_action_just_pressed("dash") and not has_dashed:
-			velocity.x *= dash_multiplier
+			velocity.x += Input.get_axis("move_left","move_right") * dash_multiplier
 			has_dashed = true
 			has_dashed = await refresh_cooldown()
 	else:
@@ -38,32 +40,34 @@ func input() -> Vector2:
 	return input_dir
 
 func jump():
-	if Input.is_action_just_pressed("jump") and not has_jumped and is_on_floor:
+	if Input.is_action_just_pressed("jump") and not has_jumped:
 		has_jumped = true
 		velocity.y = jump_velocity
-	elif Input.is_action_pressed("super_jump") and not has_super_jumped:
-		while is_on_floor():
-			if await charge_super_jump():
-				velocity.y = jump_velocity
-				has_super_jumped = true
-				has_super_jumped = await refresh_cooldown()
-				
-		
+	elif velocity.y != 0:
+		has_jumped = true
+		velocity.y += grav
 	else:
-		
 		velocity.y += grav
 		if is_on_floor():
 			has_jumped = false
+	
+	charge_super_jump()
+	if can_super_jump and Input.is_action_just_released("super_jump"):
+		has_jumped = true
+		can_super_jump = false
+		velocity.y = jump_velocity * 1.5
 
-func refresh_cooldown(has_condition: bool = true) -> bool:
+func refresh_cooldown() -> bool:
 	await get_tree().create_timer(cooldown).timeout
 	return false
 
+func charge_super_jump():
+	var charge_time = 0.7
+	if Input.is_action_just_pressed("super_jump"):
+		has_jumped = true
+		$PlayerSuperJumpTimer.start(charge_time)
+	if Input.is_action_just_released("super_jump"):
+		$PlayerSuperJumpTimer.stop()
 
-# TODO: Debug Super Jump
-func charge_super_jump() -> bool:
-	while Input.is_action_pressed("super_jump"):
-		await get_tree().create_timer(cooldown).timeout
-		if Input.is_action_just_released("super_jump"):
-			return true
-	return false
+func _on_player_super_jump_timer_timeout():
+	can_super_jump = true
