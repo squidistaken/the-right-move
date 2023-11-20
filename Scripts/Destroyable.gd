f2070a90908c90b8b7061df21d63d8ff227b0c1a
Marcus Persson <marcusper2003@gmail.com>
2023-11-21 00:57:22 +0100

    learned how 2 metsys

--- Scripts/Destroyable.gd
+++ Scripts/Destroyable.gd
@@ -3,15 +3,15 @@ extends Node
 var can_action = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if can_action == true:
 		if Input.is_action_just_pressed("action"):
 			queue_free()
 
 
-func _on_destroyable_area_body_entered(body):
+func _on_destroyable_area_body_entered(_body):
 	can_action = true
 
 
-func _on_destroyable_area_body_exited(body):
+func _on_destroyable_area_body_exited(_body):
 	can_action = false
